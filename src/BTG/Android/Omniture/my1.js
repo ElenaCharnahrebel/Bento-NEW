bff8c8c73e28ba9dbf6d88abb7c13aabf243b940
ElenaCharnahrebel <elena_charnahrebel@epam.com>
2014-12-19 13:45:53 +0300

    test

---
+++ src/BTG/Android/Omniture/my1.js
@@ -0,0 +1,23 @@
+[
+	{
+		"audienceManagerEnabled" : "true",
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarndblabla",
+		"omnitureChannel" : "TestChannellala",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USDlala",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sclala.mtv.com",
+		"omnitureVisitorID" : "2lala",
+		"omnitureDebugLogging" : "true",
+		"omnitureOfflineTrackingEnabled" : "true",
+		"omnitureOfflineHitLimit" : 1000,
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true",
+		"omnitureTimetrackingEnabled" : "true",
+		"appIsCoppaCompliant" : "false"
+    	}
+]
+
+
