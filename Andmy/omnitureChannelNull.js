9e4e2bfb9485a72bc7a8dd0eba54f3cba2954a4a
ElenaCharnahrebel <elena_charnahrebel@epam.com>
2014-12-20 15:43:22 +0300

    hgf

---
+++ Andmy/omnitureChannelNull.js
@@ -0,0 +1,28 @@
+
+[
+	{
+		"audienceManagerEnabled" : "true",
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "sc.mtv.com",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+		"omnitureOfflineTrackingEnabled" : "true",
+		"omnitureOfflineHitLimit" : 1000,
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true",
+		"omnitureContextData" : {
+			"v.appContext1":"test1",
+			"v.appContext2":"test2", 
+			"dummyContext12":"app12",
+			"nick.game": "app1"
+		},
+		"omnitureTimetrackingEnabled" : "true",
+		"appIsCoppaCompliant" : "false"
+	}
+]
