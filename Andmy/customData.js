64e07733226a6f64b679be9b28e76696f1037521
ElenaCharnahrebel <elena_charnahrebel@epam.com>
2014-12-20 16:40:17 +0300

    nb

---
+++ Andmy/customData.js
@@ -0,0 +1,27 @@
+[
+	{
+		"linkName" : "dev/bento/link",
+		"pageName" : "dev/bento/testrig",
+		"channel"  : "remote sandbox channel-test",
+		"prop1"    : "remote value of prop 1",
+        "prop11"    : "remote value of prop 11",
+        "prop74"    : "remote value of prop 74",
+        "prop75"    : "remote value of prop 75",
+        "prop76"    : "remote value of prop 76",
+        "prop111"    : "remote value of prop 111",
+        "eVar1"    : "remote value of evar 1",
+        "eVar75"    : "remote value of evar 75",
+        "eVar76"    : "remote value of evar 76",
+        "eVar74"    : "remote value of evar 74",
+        "eVar111"    : "remote value of evar 111",
+        "eVar300"    : "remote value of evar 300",
+        "eVar9"	     : "",
+        "events" : "event1,event2,event3",
+        "contextData"   : {
+			"contextDataKey1trackpage" : "testcontextvalue1trackpage",
+			"contextDataKey2trackpage" : "testcontextvalue2trackpage",
+			"m.testcontext1" : "testcontext1",
+			"nick.game"	: "nickgamecontext"
+        }
+	}
+]
