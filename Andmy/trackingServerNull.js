96d2bad82d1e33de768685e9c170ebfdb0d28d99
ElenaCharnahrebel <elena_charnahrebel@epam.com>
2014-12-20 15:25:12 +0300

    gfd

---
+++ Andmy/trackingServerNull.js
@@ -0,0 +1,28 @@
+
+[
+	{
+		"audienceManagerEnabled" : "true",
+		"omnitureEnabled" : "true",
+		"omnitureSuite"   : "viarnd",
+		"omnitureChannel" : "Test Rig Channel",
+		"omnitureCharSet" : "ISO-8859-1",
+		"omnitureCurrencyCode" : "USD",
+		"omnitureSSL" : "false",
+		"omnitureLifecycleSessionTimeout" : "300",
+		"omnitureTrackingServer" : "",
+		"omnitureVisitorID" : "",
+		"omnitureDebugLogging" : "true",
+		"omnitureOfflineTrackingEnabled" : "true",
+		"omnitureOfflineHitLimit" : 1000,
+		"demdexEnabled" : "true",
+		"TVEEnabled" : "true",
+		"omnitureContextData" : {
+			"v.appContext1":"test1",
+			"v.appContext2":"test2", 
+			"dummyContext12":"app12",
+			"nick.game": "app1"
+		},
+		"omnitureTimetrackingEnabled" : "true",
+		"appIsCoppaCompliant" : "false"
+	}
+]
