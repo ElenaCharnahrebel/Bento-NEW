5cba518a5a8492ac9859417f73360e972333ba65
ElenaCharnahrebel <elena_charnahrebel@epam.com>
2015-03-04 14:48:15 +0300

    upd

--- Andmy/customData.js
+++ Andmy/customData.js
@@ -1,17 +1,17 @@
 [
     {
-        "pageName": "dev/bento/testrig",
-        "linkName": "dev/bento/link",
-        "prop1": "testValueProp1",
-        "eVar1": "testValueEvar1",
-        "eVar32": "testValueEvar32",
-        "prop11": "testValueProp11",
-        "eVar11": "testValueEvar11",
-        "prop111": "testValueProp111",
-        "eVar111": "testValueEvar111",
-        "prop74": "testValueProp74",
-        "eVar74": "testValueEvar74",
-        "prop75": "testValueProp75",
-        "eVar75": "testValueEvar75",
+        "pageName":"dev/bento/testrig",
+        "linkName":"dev/bento/link",
+        "prop1":"testValueProp1",
+        "eVar1":"testValueEvar1",
+        "eVar32":"testValueEvar32",
+        "prop11":"testValueProp11",
+        "eVar11":"testValueEvar11",
+        "prop111":"testValueProp111",
+        "eVar111":"testValueEvar111",
+        "prop74":"testValueProp74",
+        "eVar74":"testValueEvar74",
+        "prop75":"testValueProp75",
+        "eVar75": testValueEvar75"
     }
 ]
